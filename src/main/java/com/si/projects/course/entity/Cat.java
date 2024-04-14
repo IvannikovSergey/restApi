f38f4ae58e0434dade61a64cf05e3f795ff0f464
IvannikovSergey <nagoymail@gmail.com>
2024-04-14 20:30:31 +0500

    интеграция PostgreSQL в REST

--- src/main/java/com/si/projects/course/entity/Cat.java
+++ src/main/java/com/si/projects/course/entity/Cat.java
@@ -1,15 +1,26 @@
 package com.si.projects.course.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @Setter
+@Entity
+@Table(name = "cats")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(unique = true)
     String name;
+
     int age;
+
     int weight;
 }
